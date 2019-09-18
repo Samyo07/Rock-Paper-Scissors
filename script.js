8ff55d4f230f8dd43f5427d0c0132eb2ca3589e8
Samuel Bennion <bennionsamuel@gmail.com>
2019-09-18 16:54:55 -0600

    Finsh rock paper scissors game. Added comments and cleaned up a few lines of code.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function computerPlay(){
+alert("enter game() in console to play Rock, Paper, Scissors.")
+
+function computerPlay(){ //Determines if the computer will select rock, paper, or scissors randomly.
     let selection = Math.floor(Math.random() * (2) + 1);
     switch(selection){
         case 3:
@@ -16,13 +18,13 @@ function computerPlay(){
     }
     return selection;
 }
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){ //deterimes who wins (Computer or Human) round based on arguements passed (rock, paper, or scissors) 
     playerSelection = playerSelection.toLowerCase(); //Make sure isn't case sensitive
-    computerSelection = computerSelection.toLowerCase();
+    computerSelection = computerSelection;
     if(playerSelection == "rock"){
         if(computerSelection == "rock"){
             console.log("It's a tie");
-            return "tie"
+            return "tie" //Each return case from here down will include either "tie", "loss", or "win"
         } else if(computerSelection == "paper"){
             console.log("You lose. Paper beats rock");
             return "loss";
@@ -62,17 +64,17 @@ function playRound(playerSelection, computerSelection){
         return "Something went really wrong";
     }
 }
-function game(){
-    let wins = 0;
+function game(){ //sets up game of rock paper scissors with 5 rounds to play. Use this function in console to play game.
+    let wins = 0; //tally wins and losses for end of game
     let losses = 0;
-    for(let x = 0; x < 5; x++){
+    for(let x = 0; x < 5; x++){ //5 rounds
         let cpSelect = computerPlay();
-        let w = playRound(prompt("Choose rock, paper, or scissors"), cpSelect);
-        if(w == "win"){
+        let gameResult = playRound(prompt("Choose rock, paper, or scissors"), cpSelect);
+        if(gameResult == "win"){
             wins++;
-        } else if(w == "loss"){
+        } else if(gameResult == "loss"){
             losses++;
-        } else if(w == "tie"){
+        } else if(gameResult == "tie"){
             //do nothing...
         } else{
             console.log("error with win/loss")
@@ -80,9 +82,12 @@ function game(){
     }
     if(wins > losses){
         alert("You won the game!\n" + wins + " Wins | " + losses + " losses.");
+        return "You won the game!\n" + wins + " Wins | " + losses + " losses."; //Also returns result so game result is printed to the console.
     } else if(losses > wins){
-        return ("You lost the game\n" + wins + " Wins | " + losses + " losses.");
+        alert ("You lost the game\n" + wins + " Wins | " + losses + " losses.");
+        return "You lost the game\n" + wins + " Wins | " + losses + " losses.";
     } else{
+        alert ("Tie\n" + wins + " Wins | " + losses + " losses.");
         return "Tie\n" + wins + " Wins | " + losses + " losses.";
     }
 }
